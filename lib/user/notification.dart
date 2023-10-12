e1fad49ea3d3601c69cc47fac1d80cda677e2a76
harshrajurkar <harshrajurkar201@gmail.com>
2023-10-12 21:26:07 +0530

    stream builder

--- lib/user/notification.dart
+++ lib/user/notification.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sahayak/Themeconst.dart';
 
@@ -11,18 +12,38 @@ class notification extends StatefulWidget {
 class _notificationState extends State<notification> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: kbackgroundcolor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                "sent requests",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          ],
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    return Scaffold(
+      //backgroundColor: kbackgroundcolor,
+      body: Center(
+        child: FutureBuilder<QuerySnapshot>(
+          future: firestore.collection('service_requests').get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return Text('No documents found');
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var document = snapshot.data!.docs[index];
+                var data = document.data() as Map<String, dynamic>;
+                // You can access fields of the document here
+                var date = data['createdAt'];
+                var Issue = data['issue'];
+
+                return ListTile(
+                  title: Text('Title: $date'),
+                  subtitle: Text('Issue: $Issue'),
+                );
+              },
+            );
+          },
         ),
       ),
     );
