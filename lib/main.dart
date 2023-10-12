891bb468c60fe8e2ccc867ea13eeb5c5056d3f35
YashIngole <yashingole1000@gmail.com>
2023-10-12 11:59:36 +0530

    welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sahayak/auth%20svc/helper.dart';
+import 'package:sahayak/auth%20ui/welcome_ui.dart';
 import 'package:sahayak/constants.dart';
 import 'package:sahayak/firebase_options.dart';
 import 'package:sahayak/user/home.dart';
-import 'auth ui/OnboardingPage.dart';
+
 //import 'package:sahayak/user/SearchWorkers.dart';
 
 void main() async {
@@ -64,6 +65,6 @@ class _MyAppState extends State<MyApp> {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: OnboardingPage());
+        home: WelcomePage());
   }
 }
