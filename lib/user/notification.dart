79eb99c946941375b6bc9e187fcb0307131c170c
harshrajurkar <harshrajurkar201@gmail.com>
2023-10-12 22:19:55 +0530

    notification UI polished

--- lib/user/notification.dart
+++ lib/user/notification.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sahayak/Themeconst.dart';
 
 class notification extends StatefulWidget {
@@ -14,7 +15,20 @@ class _notificationState extends State<notification> {
   Widget build(BuildContext context) {
     final FirebaseFirestore firestore = FirebaseFirestore.instance;
     return Scaffold(
-      //backgroundColor: kbackgroundcolor,
+      backgroundColor: kbackgroundcolor,
+      appBar: AppBar(
+        backgroundColor: kbackgroundcolor,
+        title: Text("Sent Requests", style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ), // Replace "your_custom_icon" with the icon you want to use
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Center(
         child: FutureBuilder<QuerySnapshot>(
           future: firestore.collection('service_requests').get(),
@@ -36,10 +50,22 @@ class _notificationState extends State<notification> {
                 // You can access fields of the document here
                 var date = data['createdAt'];
                 var Issue = data['issue'];
+                var status = data['AcceptStatus'];
 
                 return ListTile(
-                  title: Text('Title: $date'),
-                  subtitle: Text('Issue: $Issue'),
+                  title: Text(
+                    'Date: $date',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    'Issue: $Issue',
+                    style: const TextStyle(color: Colors.white70),
+                    maxLines: 2,
+                  ),
+                  trailing: Text(
+                    ' $status',
+                    style: TextStyle(color: Colors.white70),
+                  ),
                 );
               },
             );
